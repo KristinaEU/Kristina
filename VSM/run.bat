a3993db703332fb0f7d2acca49b67c5194a60522
Gregor Mehlmann <mehlmann@hcm-lab.de>
2016-02-04 13:35:39 +0100

    vsm update

--- VSM/run.bat
+++ VSM/run.bat
@@ -1,3 +1,3 @@
-java -cp "./lib/*;./bld/*" eu.kristina.vsm.VSMControlService http://137.250.171.231:11223/
+java -cp "./lib/*;./bld/*" eu.kristina.vsm.VSMRestFulServer http://137.250.171.231:11223/
 
 pause
