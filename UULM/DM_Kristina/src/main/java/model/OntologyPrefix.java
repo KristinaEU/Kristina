815429a8cde24709a6f63f3dfc55de542d7c8f5e
LouisaPragst <louisa.pragst@uni-ulm.de>
2016-09-14 14:43:09 +0200

    updated LA ontology prefix

--- UULM/DM_Kristina/src/main/java/model/OntologyPrefix.java
+++ UULM/DM_Kristina/src/main/java/model/OntologyPrefix.java
@@ -1,7 +1,7 @@
 package model;
 
 public class OntologyPrefix {
-	public final static String act = "http://kristina-project.eu/ontologies/la/act#";
+	public final static String act = "http://kristina-project.eu/ontologies/la/action#";
 	public static final String onto = "http://kristina-project.eu/ontologies/responses#";
 	public static final String dialogue = "http://kristina-project.eu/ontologies/dialogue_actions#";
 	public static final String ontoLA = "http://kristina-project.eu/ontologies/la/onto#";
